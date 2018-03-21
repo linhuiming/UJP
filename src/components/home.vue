6305c340ad8841a349fd92b04461fcc3678e3c50
linhuiming <1083475031@qq.com>
2018-03-21 08:41:25 +0800

    配置反向代理

---
+++ src/components/home.vue
@@ -0,0 +1,30 @@
+<template>
+	
+</template>
+
+<script type="text/ecmascript-6">
+ 
+  import axios from "axios";
+
+  export default{
+		name:'home',
+		data(){
+			return{
+         
+			}
+		},
+    mounted(){
+      axios.get("/api/v5/home").then(res =>{
+          
+      }).catch(error =>{
+
+      })
+    
+    },
+    
+	}
+</script>
+
+<style lang="scss" scoped>
+ 
+</style>
