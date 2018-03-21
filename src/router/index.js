c50c977a9acff74ad00430fe2c9dbe2f98091d9e
linhuiming <1083475031@qq.com>
2018-03-21 08:55:14 +0800

    反向代理和路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from "../components/home"			//主页
+import Register from "../components/register"	//注册
+import Cart from "../components/cart"			//购物车
+import List from "../components/list"			//列表
+import Login from "../components/login"			//登录
+import Detail from "../components/detail"		//详情
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/cart',
+      component: Cart
+    },
+    {
+      path: '/list',
+      component: List
+    },{
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/detail/:id',
+      component:Detail
+    },
+    {
+      path: '/*',
+      redirect:"/home"
     }
   ]
 })
